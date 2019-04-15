7a72a68bf815273eef8147b3d962fc68b181cc50
sitahlatif <sitah27@hotmail.com>
2019-04-15 20:18:17 +0300

    Structure_Component

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,20 +2,22 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 import * as actionCreators from "../store/actions";
+import Playarea from "./PlayArea";
 
 class HomePage extends Component {
   render() {
     const { msg } = this.props;
     return (
       <div className="my-4">
-        <h1>state : {msg}</h1>
+        {/* <h1>state : {msg}</h1>
         <br />
         <button
           onClick={() => this.props.sayHi("pirate")}
           className="btn btn-info"
         >
           Click Me
-        </button>
+        </button> */}
+        <Playarea />
       </div>
     );
   }
