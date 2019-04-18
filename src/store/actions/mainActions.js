1acaec7a335d8ec3b6d31f9a73a90d32160afeab
anassu1400 <anassu1400@gmail.com>
2019-04-18 12:52:21 +0300

    redux store done for building blocks

--- src/store/actions/mainActions.js
+++ src/store/actions/mainActions.js
@@ -1,1 +1,16 @@
 import * as actionTypes from "./types";
+
+export const addBuildingBlock = block => {
+  console.log("TCL: block", block);
+  return {
+    type: actionTypes.ADD_BLOCK,
+    payload: block
+  };
+};
+
+export const setBuildingBlocks = newBB => {
+  return {
+    type: actionTypes.SET_BB,
+    payload: newBB
+  };
+};
