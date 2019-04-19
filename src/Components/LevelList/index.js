846ae616c68aefe983dd865b99073be62f296d60
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-04-20 01:35:50 +0300

    Add the footer in levels page

--- src/Components/LevelList/index.js
+++ src/Components/LevelList/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import pirateBird from "../../assets/images/pirateBird.png";
+
+// Fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 // Components
 import LevelCard from "./LevelCard";
 
@@ -20,6 +25,19 @@ class LevelList extends Component {
           {this.props.lang[0] && this.props.lang[0].name}
         </h2>
         <div className="row justify-content-center">{levelCards}</div>
+        <br />
+
+        <div className="row my-5 justify-content-center">
+          {/* <h3 style={{ color: "#c5c5c5" }}>
+            صنع بـ بواسطة فريق السعادة من معسكر طويق الأول
+          </h3> */}
+          <span className="footer"> صنع بـ </span>
+          <FontAwesomeIcon className="footer footer-heart" icon={faHeart} />
+          <span className="footer">
+            {" "}
+            بواسطة فريق السعادة في معسكر طويق الأول
+          </span>
+        </div>
       </div>
     );
   }
